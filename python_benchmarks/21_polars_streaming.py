3c35f9eb3cf806218303209ae13e02df2140cf16
ritchie <ritchie46@gmail.com>
2024-02-15 21:14:37 +0100

    improve polars query

--- python_benchmarks/21_polars_streaming.py
+++ python_benchmarks/21_polars_streaming.py
@@ -1,20 +1,17 @@
-import polars
+import polars as pl
 
 
 def polars_streaming(fname):
-    dtypes = {f"column{i}": (polars.UInt32 if i == 0 else polars.Float32())
+    dtypes = {f"column{i}": (pl.UInt32 if i == 0 else pl.Float32())
               for i in range(9)}
-    return (polars.scan_csv(fname, dtypes=dtypes)
-                  .select(
-                      polars.sum_horizontal(polars.all()
-                                                  .exclude("0")
-                                                  .pow(2)
-                      ).sqrt()
-                       .alias("sum_sq"))
-                  .filter(polars.col("sum_sq") < 1e16)
-                  .count()
-                  .collect(streaming=True)
-    )
+    return (pl.scan_csv(fname, dtypes=dtypes)
+            .filter(pl.sum_horizontal(pl.all()
+                                      .exclude("column0")
+                                      .pow(2)
+                                      ).sqrt() < 1e16)
+            .select(pl.len())
+            .collect(streaming=True)
+            )
 
 
 if __name__ == "__main__":
